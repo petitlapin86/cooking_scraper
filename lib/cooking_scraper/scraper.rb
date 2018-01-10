45d3a571d3478b392dbbf95f45c724df13944f07
petitlapin86 <skinnerpa86@yahoo.com>
2018-01-10 11:02:06 -0500

    adding scraped elements

--- lib/cooking_scraper/scraper.rb
+++ lib/cooking_scraper/scraper.rb
@@ -34,64 +34,88 @@ end
 
 def self.scrape_aries
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/aries.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 def self.scrape_taurus
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/taurus.html",  "Accept-Encoding" => "plain"))
   binding.pry
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 def self.scrape_gemini
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/gemini.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 
 def self.scrape_cancer
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/cancer.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 def self.scrape_leo
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/leo.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 def self.scrape_virgo
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/virgo.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 def self.scrape_libra
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/libra.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 def self.scrape_scorpio
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/scorpio.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 def self.scrape_sagittarius
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/sagittarius.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 def self.scrape_capricorn
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/capricorn.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 def self.scrape_aquarius
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/aquarius.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 def self.scrape_pisces
   doc = Nokogiri::HTML(open("https://www.astrology.com/horoscope/daily/pisces.html",  "Accept-Encoding" => "plain"))
-  horoscope = doc.search("p").text
+  sign = self.new
+  sign.horoscope = doc.search("p").text
+  sign
 end
 
 
