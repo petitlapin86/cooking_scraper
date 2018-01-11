400713c52c42e1d2ce5e317b5455cc83c9cf93e0
petitlapin86 <skinnerpa86@yahoo.com>
2018-01-11 15:14:03 -0500

    making sure all files have been updated with new name

---
+++ lib/horoscope_scraper.rb
@@ -0,0 +1,13 @@
+#require 'open-uri'
+#require 'nokogiri'
+require 'pry'
+#require 'colorize'
+
+
+require_relative './horoscope_scraper/version'
+require_relative './horoscope_scraper/cli'
+require_relative './horoscope_scraper/scraper'
+
+module HoroscopeScraper
+  # Your code goes here...
+end
